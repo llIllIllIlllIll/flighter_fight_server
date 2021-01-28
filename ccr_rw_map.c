903671600fbefb921bf20d6872b964362c3ead9b
Yang Xiao <yx1998@sjtu.edu.cn>
2021-01-28 02:27:23 -0500

    finish ccr_rw_map

---
+++ ccr_rw_map.c
@@ -0,0 +1,70 @@
+#include "ccr_rw_map.h"
+int ccr_rw_map_init(ccr_rw_map * cmap){
+	int ret = pthread_rwlock_init(&cmap->rwlock,NULL);
+	if(ret != 0){
+		fprintf(stderr,"Error when initializing rwlock.\n");
+		return -1;
+	}
+	//cmap->rwlock = PTHREAD_RWLOCK_INITIALIZER;
+	for(int i = 0; i < 100; i++){
+		cmap->slots[i] = NULL;
+	}
+	return 0;
+}
+int ccr_rw_map_insert(ccr_rw_map * cmap,uint32_t k,uint32_t v){
+	int ret = pthread_rwlock_wrlock(&cmap->rwlock);
+	if(ret != 0){
+		fprintf(stderr,"Error when getting wrlock.\n");
+		return -1;
+	}
+	slot * new_slot = (slot *)malloc(sizeof(slot));
+	if(new_slot == NULL){
+		fprintf(stderr,"Error when calling malloc.\n");
+		pthread_rwlock_unlock(&cmap->rwlock);
+		return -1;
+	}
+	new_slot->next = NULL;
+	new_slot->key = k;
+	new_slot->val = v;
+	int slot_n = HASH(k);
+	if(cmap->slots[slot_n] == NULL){
+		cmap->slots[slot_n] = new_slot;
+		pthread_rwlock_unlock(&cmap->rwlock);
+		return 0;
+	}
+	else{
+		slot * cur_slot = cmap->slots[slot_n];
+		while(cur_slot->next != NULL){
+			if(cur_slot->key == k){
+				pthread_rwlock_unlock(&cmap->rwlock);
+				fprintf(stderr,"Error when inserting <%u,%u>: <%u,%u> already exists.\n",k,v,cur_slot->key,cur_slot->val);
+				return -1;
+			}
+			cur_slot = cur_slot->next;
+		}
+		cur_slot->next = new_slot;
+		pthread_rwlock_unlock(&cmap->rwlock);
+		return 0;
+	}
+}
+int ccr_rw_map_query(ccr_rw_map * cmap,uint32_t k,uint32_t * v){
+	int ret = pthread_rwlock_rdlock(&cmap->rwlock);
+	if(ret != 0){
+		fprintf(stderr,"Error when getting rdlock.\n");
+		return -2;
+	}
+	slot * target_slot = cmap->slots[HASH(k)];
+	while(target_slot != NULL){
+		if(target_slot->key == k){
+			*v =  target_slot->val;
+			pthread_rwlock_unlock(&cmap->rwlock);
+			return 0;
+		}
+		else{
+			target_slot = target_slot->next;
+		}
+	}
+	pthread_rwlock_unlock(&cmap->rwlock);
+	return -1;
+}
+
--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include "flighter_msg.h"
 #include "net.h"
+#include "ccr_rw_map.h"
 #include <stdio.h>
 sbuf_t sbuf;
+ccr_rw_map cmap;
 // each thread be responsible for a room 
 // may consider coroutine later
 void * room_thread(void * vargp){
@@ -26,13 +28,14 @@ int main(int argc,char * argv[]){
 	clients_listenfd = open_listenfd(argv[2]);
 	int poolsize = atoi(argv[2]);
 	sbuf_init(&sbuf,poolsize);
+	ccr_rw_map_init(&cmap);
 	for(int i = 0; i < poolsize;i++){
-		pthread_create(&tid,NULL,thread,NULL);
+		pthread_create(&tid,NULL,room_thread,NULL);
 	}
 
 	while(1){
 		clientlen = sizeof(struct sockaddr_storage);
-		connfd = accept(listenfd,(SA *)&clientaddr,&clientlen);
+		connfd = accept(roomserver_listenfd,(SA *)&clientaddr,&clientlen);
 		sbuf_insert(&sbuf,connfd);
 	}
 	return 0;
